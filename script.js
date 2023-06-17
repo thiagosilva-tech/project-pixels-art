3a1f71421761b25a29eff232ad80078f92f1174a
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-06-17 12:17:57 -0300

    fix: erros eslint

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-window.onload = function (params) {
-    const colors = document.querySelectorAll('.color');
-    function changeBackgroundColor() {
-        for (let index = 0; index < colors.length; index += 1) {
-            const color = colors[index].innerText;
-            colors[index].style.backgroundColor = color;
-            colors[index].innerText = '';            
-        }
+window.onload = function () {
+  const colors = document.querySelectorAll('.color');
+  function changeBackgroundColor() {
+    for (let index = 0; index < colors.length; index += 1) {
+      const color = colors[index].innerText;
+      colors[index].style.backgroundColor = color;
+      colors[index].innerText = '';
     }
-    changeBackgroundColor();
-}
+  }
+  changeBackgroundColor();
+};
